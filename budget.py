2590e714a2e6a7d162c5dff4ed9759f38c24ea0b
Bowen_Local <tylerbowen40@gmail.com>
2024-09-19 20:11:25 -0400

    Changes for camel case, improper dictionary add, and musings on enumeration

--- budget.py
+++ budget.py
@@ -3,12 +3,12 @@
 
 """ What is an easy way to turn an expense into a question? I want to be able to make some of these variable i.e. ElectricBill = float(input("What was your electric bill this month?")) Feeds my dictionary amount for ElectricBill which then combines with other key values to create my object MonthlyExpenses. Can I  have that combination of fixed and variable items wwithin a dictionary, or should I create a string of elif statements and broaden my initial UseDecision statement?   """
 
-MonthlyExpenses = items(Mortgage 3034.88: , HOAPayment : 73, CarPayment : 1670.22, Audible : 15.99, BabySittingFee : 300, ChurchDonation : 103.29, TogoDonation : 60, ChatGPT : 20, Netflix : 15.49, WSJ : 4, NYT : 4, Adobe : 19.99, DisneyPlus : 24.99, KindleUnlimited : 11.99, Internet : 105.91, Spotify : 16.99, AllstateInsurance : 200.10 CellPhone : )
-
-MonthlyIncomeStream = items(PayCheck : 9300, Disability : 4202)
-
-VariableExpenses = items(ElectricBill : , Sanitation : , WaterBill : ,)
-
+#MonthlyExpenses = items(Mortgage 3034.88: , HOAPayment : 73, CarPayment : 1670.22, Audible : 15.99, BabySittingFee : 300, ChurchDonation : 103.29, TogoDonation : 60, ChatGPT : 20, Netflix : 15.49, WSJ : 4, NYT : 4, Adobe : 19.99, DisneyPlus : 24.99, KindleUnlimited : 11.99, Internet : 105.91, Spotify : 16.99, AllstateInsurance : 200.10 CellPhone : )
+#
+#MonthlyIncomeStream = items(PayCheck : 9300, Disability : 4202)
+#
+#VariableExpenses = items(ElectricBill : , Sanitation : , WaterBill : ,)
+#
 print ("Welcome to wasting less money!")
 InitialFunds = float(input("How much money is in your combined accounts?"))
 MonthlyIncome = float(input("What is your monthly base income?"))#move this to a dict
